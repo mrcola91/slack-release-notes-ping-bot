020c296ca78235aa89614d60a7baf75b14d37b41
Michael Cola <cola@ediphi.com>
2024-07-18 16:04:27 -0500

    es6 conversion

--- api/slack.js
+++ api/slack.js
@@ -1,7 +1,6 @@
-const { WebClient } = require('@slack/web-api');
-const { App } = require('@slack/bolt');
-const { Octokit } = require('@octokit/rest');
-const fetch = require('node-fetch');
+import { WebClient } from '@slack/web-api';
+import { App } from '@slack/bolt';
+import { Octokit } from '@octokit/rest';
 
 // Slack and GitHub tokens
 const slackToken = process.env.SLACK_BOT_TOKEN;
